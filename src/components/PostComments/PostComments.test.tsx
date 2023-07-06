c4ee5874e734ba60780728288b73f72077f562c4
Felipe Espinoza <domingues.espinoza@outlook.com>
2023-07-06 12:00:16 -0300

    Correção de erros apresentados no teste

--- src/components/PostComments/PostComments.test.tsx
+++ src/components/PostComments/PostComments.test.tsx
@@ -2,29 +2,29 @@ import { fireEvent, render, screen } from '@testing-library/react';
 import PostComment from '.';
 
 describe('Teste para o componente PostComment', () => {
-    it('Deve renderizar o componente corretamente', () => {
-        render(<PostComment/>);
+    test('Deve renderizar o componente corretamente', () => {
+        render(<PostComment/>)
         expect(screen.getByText('Comentar')).toBeInTheDocument();
+    })
+
+    test('Deve adicionar dois comentários', () => {
+        render(<PostComment />);
+
+        // Adiciona o primeiro comentário
+        fireEvent.change(screen.getByTestId('campo-comentario'), {
+            target: {
+                value: 'Comentário adicionado via testes',
+            }
+        });
+        fireEvent.click(screen.getByTestId('btn-postar'));
+
+        // Adiciona o segundo comentário
+        fireEvent.change(screen.getByTestId('campo-comentario'), {
+            target: {
+                value: 'Segundo comentário adicionado via testes',
+            }
+        });
+        fireEvent.click(screen.getByTestId('btn-postar'));
+
     });
-
-    it('Adiciona dois comentários', () =>{
-        render(<PostComment/>);
-
-    fireEvent.change(screen.getByTestId('textarea'), {
-        target: {
-            value: 'Comentário 1 adicionado',
-        }
-    });
-    fireEvent.click(screen.getByTestId('button'));
-
-    fireEvent.change(screen.getByTestId('textarea'), {
-        target: {
-            value: 'Comentário 2 adicionado',
-        }
-    });
-    fireEvent.click(screen.getByTestId('button'));
-
-
-    expect(screen.getAllByTestId('comments')).toHaveLength(2);
-});
-});
+})
